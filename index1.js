edadaf0d7f8b6af4d9304bbb6df1cb1aecb6640d
kenlok07 <72095470+kenlok07@users.noreply.github.com>
2021-02-01 08:06:07 -0500

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,21 @@
+// [START maps_add_map]
+// Initialize and add the map
+function initMap() {
+  // [START maps_add_map_instantiate_map]
+  // The location of University of Waterloo
+  const uw = { lat: 43.4723, lng: -80.5449 };
+  // The map, centered at U of W
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uw,
+  });
+  // [END maps_add_map_instantiate_map]
+  // [START maps_add_map_instantiate_marker]
+  // The marker, positioned at U of W
+  const marker = new google.maps.Marker({
+    position: uw,
+    map: map,
+  });
+  // [END maps_add_map_instantiate_marker]
+}
+// [END maps_add_map]
