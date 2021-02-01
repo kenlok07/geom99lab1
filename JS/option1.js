ceaca1751c556a8bb62f2235e09229bdc5854007
kenlok07 <72095470+kenlok07@users.noreply.github.com>
2021-02-01 12:16:47 -0500

    Add files via upload

--- JS/option1.js
+++ JS/option1.js
@@ -1,16 +1,21 @@
+// [START maps_add_map]
 // Initialize and add the map
 function initMap() {
-  // The location of my first secondary school, United Christian College Kowloon East (Hong Kong)
+  // [START maps_add_map_instantiate_map]
+  // The location of University of Waterloo
   const uccke = { lat: 22.330807, lng: 114.22238 };
-  // The map, centered at uccke
+  // The map, centered at U of W
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
+    zoom: 16,
     center: uccke,
   });
-  // The marker, positioned at uccke
+  // [END maps_add_map_instantiate_map]
+  // [START maps_add_map_instantiate_marker]
+  // The marker, positioned at U of W
   const marker = new google.maps.Marker({
     position: uccke,
     map: map,
   });
+  // [END maps_add_map_instantiate_marker]
 }
-
+// [END maps_add_map]
