72621011c5ea944a67f7847add3c0193fdfca06f
kenlok07 <72095470+kenlok07@users.noreply.github.com>
2021-02-01 11:25:29 -0500

    Add files via upload

--- JS/option1.js
+++ JS/option1.js
@@ -2,30 +2,15 @@
 function initMap() {
   // The location of my first secondary school
   const uccke = { lat: 22.330807, lng: 114.22238 };
-  // The map, centered at Uluru
+  // The map, centered at uccke
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
     center: uccke,
   });
-  // The marker, positioned at Uluru
+  // The marker, positioned at uccke
   const marker = new google.maps.Marker({
     position: uccke,
     map: map,
   });
 }
 
-// Initialize and add the map
-function initMap() {
-  // The location of my second high school
-  const ac = { lat: 44.154520, lng: -77.393800 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map2"), {
-    zoom: 14,
-    center: ac,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: ac,
-    map: map,
-  });
-}
