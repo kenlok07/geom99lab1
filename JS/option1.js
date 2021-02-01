1147a25720dc3c964ae252a93f9a4a23a51b7bf9
kenlok07 <72095470+kenlok07@users.noreply.github.com>
2021-02-01 11:38:49 -0500

    Add files via upload

--- JS/option1.js
+++ JS/option1.js
@@ -1,6 +1,6 @@
 // Initialize and add the map
 function initMap() {
-  // The location of my first secondary school
+  // The location of my first secondary school, United Christian College Kowloon East (Hong Kong)
   const uccke = { lat: 22.330807, lng: 114.22238 };
   // The map, centered at uccke
   const map = new google.maps.Map(document.getElementById("map"), {
